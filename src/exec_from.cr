5f92f207a61448c5875ad42010f8c260a3b95d61
Caspian Baska <caspianbaska@gmail.com>
2020-04-02 21:02:26 +1100

    improvement: add `clear_environment` and `environment` arguments

--- src/exec_from.cr
+++ src/exec_from.cr
@@ -3,6 +3,8 @@ module ExecFrom
     directory : String,
     command : String,
     arguments : Enumerable(String) = [] of String,
+    environment : Process::Env = nil,
+    clear_environment : Bool = false,
     input : IO | Process::Redirect = Process::Redirect::Close
   ) : NamedTuple(output: IO, exit_code: Int32)
     output = IO::Memory.new
@@ -10,6 +12,8 @@ module ExecFrom
       command: "./bin/exec_from",
       args: [directory, command].concat(arguments),
       input: Process::Redirect::Close,
+      env: environment,
+      clear_env: clear_environment,
       output: output,
       error: output,
     )
