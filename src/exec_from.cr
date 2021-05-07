647f6ff802a72479dd82281906fd2872318124bf
Caspian Baska <caspianbaska@gmail.com>
2021-05-07 15:51:40 +1000

    refactor: remove NamedTuples in favor of struct

--- src/exec_from.cr
+++ src/exec_from.cr
@@ -1,4 +1,12 @@
 module ExecFrom
+  struct Result
+    getter status : Process::Status
+    getter output : IO
+
+    def initialize(@status, @output)
+    end
+  end
+
   def self.exec_from(
     directory : String,
     command : String,
@@ -7,9 +15,9 @@ module ExecFrom
     clear_environment : Bool = false,
     capture_stderr : Bool = true,
     input : IO | Process::Redirect = Process::Redirect::Close
-  ) : NamedTuple(output: IO, exit_code: Int32)
+  ) : Result
     output = IO::Memory.new
-    result = Process.run(
+    status = Process.run(
       command: "./bin/exec_from",
       args: [directory, command].concat(arguments),
       input: Process::Redirect::Close,
@@ -19,9 +27,6 @@ module ExecFrom
       error: capture_stderr ? output : Process::Redirect::Close,
     )
 
-    {
-      output:    output,
-      exit_code: result.exit_code,
-    }
+    Result.new(status, output)
   end
 end
